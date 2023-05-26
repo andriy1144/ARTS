44e42eaa4494a441ad470ee05d3d338968d96306
Andrii Murgan <andriimurgan@gmail.com>
2023-05-26 18:18:47 +0300

    New created project

---
+++ src/main/java/com/ArtsCom/ARTSCOM/ArtscomApplication.java
@@ -0,0 +1,13 @@
+package com.ArtsCom.ARTSCOM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtscomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtscomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ArtsCom/ARTSCOM/ArtscomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ArtsCom.ARTSCOM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtscomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
