6533fcc3645540c239ac7236c1699fff0c959276
Andriy Murhan <andrijmurgan@gmail.com>
2023-06-14 10:06:23 +0300

    Add js inputs file system
    *Change script.js
    *Change addPostPage.html

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,1 +1,5 @@
-alert("Hi")
+const filesSlider = document.getElementById("filesInputSlider");
+
+function addInput() {
+    filesSlider.innerHTML += '<div class="mb-3"><label for="formFile" class="form-label">Picture 1</label>    <input class="form-control" type="file" id="formFile" name="files">  </div>';
+}
