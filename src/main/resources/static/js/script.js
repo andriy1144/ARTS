9d1c6c9c0094fc80897b0b0d7f870a6eeb8385a4
Andriy Murhan <andrijmurgan@gmail.com>
2023-06-17 17:00:05 +0300

    Add func to check form in JS
    *Change script.js

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,5 +1,24 @@
 const filesSlider = document.getElementById("filesInputSlider");
+let count = 2;
 
+//Function to add inputs for img
 function addInput() {
-    filesSlider.innerHTML += '<div class="mb-3"><label for="formFile" class="form-label">Picture 1</label>    <input class="form-control" type="file" id="formFile" name="files">  </div>';
+    filesSlider.innerHTML += '<div class="mb-3"><label for="formFile" class="form-label">Picture' + count + '</label>    <input class="form-control" type="file" id="formFile" name="files">  </div>';
+    count++;
+}
+
+//Check addPost form func
+function checkForm(el) {
+    let fil = "";
+
+    const title = el.title.value;
+    const descr = el.descr.value;
+
+    if(title == "" || descr == ""){
+        fil = " Title or descr must not be null";
+        const error = document.getElementById("error");
+        error.innerHTML = fil;
+        return false;
+    }
+    return true;
 }
